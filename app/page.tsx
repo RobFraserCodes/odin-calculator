0c4ef0cbee42aee3fcf38724966e43b233c32a45
Code Inverness <hello@rfraser.co.uk>
2023-07-31 21:42:11 +0100

    layout and state
    Fixed the layout to work well. Added state but no functionality or operators currently work.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from 'react'
+
 const digits = [7, 8, 9, 4, 5, 6, 1, 2, 3]
 const operators = ['+', '-', '*', '/']
 const equals = '='
@@ -6,8 +10,39 @@ const toggle = '+/-'
 const percent = '%'
 const decimal = '.'
 const display = '0'
+const zero = '0'
 
 export default function Home() {
+  const [display, setDisplay] = useState('0');
+  const [previous, setPrevious] = useState(null);
+  const [operator, setOperator] = useState(null);
+
+  function handleDigit(digit: number) {
+    if (display === '0') {
+      setDisplay(String(digit))
+    } else {
+      setDisplay(display + digit);
+    }
+  }
+
+  function handleOperator(nextOperator: string) {
+    if (operator !== null) {
+      handleEquals();
+    }
+    setPrevious(display);
+    setOperator(nextOperator);
+    setDisplay('0');
+  }
+
+  function handleEquals() {
+    if (previous !== null && operator !== null) {
+      const result = calculate(parseFloat(previous), parseFloat(display), operator);
+      setDisplay(String(result));
+      setPrevious(null);
+      setOperator(null);
+    }
+  }
+
   function add(a: number, b: number) {
     return a + b
   }
@@ -40,37 +75,47 @@ export default function Home() {
       <div>
         <h1 className='text-4xl text-center font-bold py-8 text-zinc-200'>The Odin Project: Calculator</h1>
       </div>
-      <div className='mx-auto container max-w-lg text-center bg-zinc-300'>
+      <div className='mx-auto container max-w-lg text-center rounded-md bg-zinc-300'>
         <div className="grid grid-cols-12">
+          {/* display response */}
           <div className='col-span-12'>
-            <p className="p-8 text-xl">{display}</p>
+            <p className="p-8 text-2xl font-semibold">{display}</p>
           </div>
-          </div>
-          <div className="grid grid-cols-9">
-            { digits.map((digit) => (
-              <div className='col-span-3'>
-                <button className='btn'>{digit}</button>
-              </div>
-            )) }
-          <div className='col-span-3'>
-            <button className='btn'>.</button>
-          </div>
-          <div className="col-span-3">
-            <button className='btn'>0</button>
-          </div>
-          <div className='col-span-3'>
-            <button className='btn'>=</button>
-          </div>
-        </div>
-        <div className="grid grid-cols-1">
-          { operators.map((operator) => (
-            <div className='col-span-1'>
-              <button className='btn'>{operator}</button>
+          {/* function buttons */}
+          <div className='col-span-9'>
+            <div className="grid grid-cols-3">
+              <button className="btn-function">{clear}</button>
+              <button className="btn-function">{toggle}</button>
+              <button className="btn-function">{percent}</button>
             </div>
-          )) }
+          </div>
+          {/* operators */}
+          <div className='col-span-3 bg-orange-400 h-16'>
+            {operators.map((operator) => (
+              <button key={operator} className="btn-operator" onClick={() => handleOperator(operator)}>{operator}</button>
+            ))}
+          </div>
+          {/* digits */}
+          <div className='col-span-9'>
+            <div className="grid grid-cols-3">
+              {digits.map((digit) => (
+                <button key={digit} className="btn" onClick={() => handleDigit(digit)}>{digit}</button>
+              ))}
+            </div>
+          </div>
+          {/* equals */}
+        </div>
+        <div className="grid grid-cols-12 items-center">
+          <div className="btn-equals col-span-3 rounded-bl-md">{decimal}</div>
+          <div className="btn-equals col-span-3" onClick={() => handleDigit(0)}>{zero}</div>
+          <div className="btn-equals col-span-6 rounded-br-md" onClick={() => handleEquals}>{equals}</div>
+        </div>
       </div>
-    </div>
+      <footer className='text-center font-thin mt-16 text-zinc-100'>
+        <p>Created by <a href="www.robfraser.dev"><span className='font-semibold'>Rob Fraser</span> </a>
+        as part of the <a href="https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/rock-paper-scissors"><span className='font-semibold'>Odin Project</span></a>
+        </p>
+      </footer>
     </main>
   )
 }
-``
