75a966c8d1f5d5e3231935341d0c045455f1b49c
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-08-01 23:31:19 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,93 +12,83 @@ const zero = 0
 
 export default function Home() {
   const [display, setDisplay] = useState('0')
-  const [previous, setPrevious] = useState([])
+  const [currentNumber, setCurrentNumber] = useState('')
+  const [previousNumber, setPreviousNumber] = useState('')
   const [operator, setOperator] = useState('')
-  const [result, setResult] = useState(0)
-  const [calculation, setCalculation] = useState([])
-
-  function setDisplayValue(value: Array<number>) {
-    let newValue = value.join('')
-    setDisplay(newValue.toString())
-  }
 
   function handleDigit(digit: number) {
-    previous.push(digit)
-    setPrevious(previous)
-    setDisplayValue(previous)
+    setCurrentNumber(currentNumber + digit.toString())
+    setDisplay(currentNumber + digit.toString())
   }
 
-  function handleOperator(operator: string) {
-    if (previous.length > 0) {
-      calculation.push(parseInt(previous.join('')))
-      calculation.push(operator)
-      setCalculation(calculation)
-      setPrevious([])
-      setOperator(operator)
+  function handleOperator(op: string) {
+    if (currentNumber && previousNumber && operator) {
+      const result = calculate(parseFloat(previousNumber), parseFloat(currentNumber), operator)
+      setPreviousNumber(result.toString())
+      setCurrentNumber('')
+      setDisplay(result.toString())
+      setOperator(op)
+    } else if (currentNumber) {
+      setPreviousNumber(currentNumber)
+      setCurrentNumber('')
+      setOperator(op)
     }
   }
 
   function handleEquals() {
-    if (previous.length > 0) {
-      calculation.push(parseFloat(previous.join('')))
-      setCalculation(calculation)
-      setPrevious([])
-      const result = calculate(calculation[0], calculation[2], calculation[1])
+    if (currentNumber && previousNumber && operator) {
+      const result = calculate(parseFloat(previousNumber), parseFloat(currentNumber), operator)
+      setPreviousNumber('')
+      setCurrentNumber(result.toString())
       setDisplay(result.toString())
-      setCalculation([])
-      setPrevious([])
       setOperator('')
     }
   }
 
   function handleClear() {
-    setPrevious([])
+    setCurrentNumber('')
+    setPreviousNumber('')
     setOperator('')
-    setCalculation([])
     setDisplay('0')
   }
 
   function handleToggle() {
-    console.log('+/-')
-    setDisplay('+/-')
+    if (currentNumber) {
+      setCurrentNumber((parseFloat(currentNumber) * -1).toString())
+      setDisplay((parseFloat(currentNumber) * -1).toString())
+    }
   }
 
   function handlePercent() {
-    console.log('%')
-    setDisplay('%')
+    if (currentNumber) {
+      setCurrentNumber((parseFloat(currentNumber) / 100).toString())
+      setDisplay((parseFloat(currentNumber) / 100).toString())
+    }
   }
 
   function handleDecimal() {
-    console.log('.')
-    setDisplay('.')
+    if (currentNumber && !currentNumber.includes('.')) {
+      setCurrentNumber(currentNumber + '.')
+      setDisplay(currentNumber + '.')
+    }
   }
 
-  function add(a: number, b: number) {
-    return a + b
-  }
-  function subtract(a: number, b: number) {
-    return a - b
-  }
-  function multiply(a: number, b: number) {
-    return a * b
-  }
-  function divide(a: number, b: number) {
-    return a / b
-  }
   function calculate(a: number, b: number, operator: string) {
     switch (operator) {
       case '+':
-        return add(a, b)
+        return a + b
       case '-':
-        return subtract(a, b)
+        return a - b
       case '*':
-        return multiply(a, b)
+        return a * b
       case '/':
-        return divide(a, b)
+        return a / b
       default:
         return 0
     }
   }
+
+  console.log(previousNumber, operator, currentNumber )
 
   return (
     <main className='bg-zinc-800 h-screen'>
@@ -106,9 +96,10 @@ export default function Home() {
         <h1 className='text-4xl text-center font-bold py-8 text-zinc-200'>Calculator</h1>
       </div>
       <div className='mx-auto container max-w-lg text-center rounded-md bg-zinc-300 shadow-xl'>
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 relative">
           {/* display response */}
           <div className='col-span-12'>
+            <p className="p-2 text-sm font-semibold text-zinc-500 text-right absolute right-0">{previousNumber} {operator} {currentNumber}</p>
             <p className="p-8 text-2xl font-semibold text-zinc-600">{display}</p>
           </div>
           {/* function buttons */}
